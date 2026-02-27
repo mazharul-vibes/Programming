24fd6d3f9e57d25634e9f57e61b1fed6942af58b
Mazharul-22151171 <22151171@student.westernsydney.edu.au>
2026-02-28 03:33:14 +1100

    About raisa

---
+++ Raisa.java
@@ -0,0 +1,10 @@
+public class Raisa {
+
+  public static void main(String[] args) {
+
+    System.out.println("Raisa's interest: Listening to music");
+    System.out.println("Raisa's favourite mountain: Mount Everest");
+    System.out.println("Raisa's prettiness: She has a beautiful smile.");
+
+  }
+}
